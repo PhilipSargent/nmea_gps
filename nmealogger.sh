188e07331b336cb15d1503396f5da6ba901e47ed
Philip Sargent <philip.sargent@gmail.com>
2024-05-15 19:24:28 +0300

    save logs witht he data

--- nmealogger.sh
+++ nmealogger.sh
@@ -1,5 +1,5 @@
 #!/bin/sh 
 # Created 2 May to get around pgrep funnies
 touch /root/nmea_data/nmealogger-started.txt
-python /root/nmea_gps/nmealogger.py  >>/root/nmea_gps/nmealogger_ok.txt 2>>root/nmea_gps/nmealogger_error.txt
+python /root/nmea_gps/nmealogger.py  >>/root/nmea_data/nmealogger_ok.txt 2>>root/nmea_data/nmealogger_error.txt
 
