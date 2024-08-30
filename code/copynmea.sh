ef6b7bbda0694daa545353aaa4e8a7f22363ec36
Philip Sargent <philip.sargent@gmail.com>
2024-08-30 15:31:39 +0300

    bug fix

--- code/copynmea.sh
+++ code/copynmea.sh
@@ -19,5 +19,7 @@ echo nmea data copy ran ok.
 # Here's the part you need to append, provided here separately for easy copy/pasting:
 # https://healthchecks.io/checks/9cac10d3-757d-4ff3-96cc-e7714825e35f/details/
 # using wget (10 second timeout, retry up to 5 times):
-wget https://hc-ping.com/9cac10d3-757d-4ff3-96cc-e7714825e35f -T 10 -t 5 -O /dev/null
+# NB OpenWRT/busybox wget does not do -t
+# wget https://hc-ping.com/9cac10d3-757d-4ff3-96cc-e7714825e35f -T 10 -t 5 -O /dev/null
+wget https://hc-ping.com/9cac10d3-757d-4ff3-96cc-e7714825e35f -T 10 -O /dev/null
 echo healthchecks.io GET ran ok
