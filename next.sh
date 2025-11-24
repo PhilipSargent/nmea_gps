21ea01948c0a7d2f7d373a6d8ca1e638155a1dda
Philip Sargent <philip.sargent@gmail.com>
2025-11-24 18:46:58 +0200

    new utility typo

--- next.sh
+++ next.sh
@@ -1,4 +1,4 @@
-!/bin/sh
+#!/bin/sh
 # Gemini
 # to see how long it is before crontab next runs nmealogger.sh 
 M=$(date +%M); S=$(date +%S); T=$(( (3 - (M % 3)) * 60 - S )); echo "$((T/60)) minutes and $((T%60)) seconds"
