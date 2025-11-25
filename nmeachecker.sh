271fa4f083e57c87cf37e27efbb95b329eeccbd4
Philip Sargent <philip.sargent@gmail.com>
2025-11-25 09:05:35 +0200

    fix

--- nmeachecker.sh
+++ nmeachecker.sh
@@ -18,20 +18,23 @@ current_time=$(date +%s)
 threshold_time=$((current_time - (threshold * 60))) # 13 minutes ago
 usual_time=$((current_time - (usual * 60))) # a minute ago
 
-# Loop through files in the directory
-overdue_updated=0  # Flag to track if any hung updated file is found
-usual_updated=0  # Flag to track if any late updated file is found
-
 root_dir="/root/" # Has to be tooted in filesystem as this runs under crontab
-#root_dir="/home/philip/gps/nmea_mirror/"
+# root_dir="/home/philip/gps/nmea_mirror/"
 
+error_file=${root_dir}nmea_logs/nmealogger_error.txt
+
+overdue_updated=1  # Flag to say they are all old
+usual_updated=1  # Flag to say they are all old
+youngest_fn="None"
+youngest_mtime="0"
 
 track=`cat ${root_dir}nmea_logs/current_nmea_file.txt`
-echo "Track id file current_nmea_file.txt says: $track"
- if [ -f ${root_dir}nmea_data/current_nmea_file.txt  ]; then
+echo "`date` Track id file current_nmea_file.txt says: $track"
+ if [ -f ${root_dir}nmea_logs/current_nmea_file.txt  ]; then
     echo "EXPECTED most recent nmea TRACK file is: `cat $track`"
     
  fi
+# format of $track is 2025-11/2025-11-21_1234.nmea
 directory=$(echo "$track" | cut -c 1-7)
 trackpath=${root_dir}nmea_data/$track
 if [ -f $trackpath  ]; then
@@ -43,7 +46,7 @@ if [ -f $trackpath  ]; then
 
     # Compare with threshold time
     if [ $file_mtime -gt $threshold_time ]; then
-       overdue_updated=1
+      overdue_updated=1
       updated=$filename
       update_dir=$directory
     fi
@@ -56,14 +59,15 @@ if [ -f $trackpath  ]; then
     fi    
 else
     echo "TRACK file $trackpath not found, searching for recent .nmea files "
-    # This checks every single .nmea file we have. Really we should just look at the
-    # current file, whos ename is in current_nmea_file.txt
+    # This checks every single .nmea file we have. 
+    # it will always find lots of old files, we are looking for just one which is younger.
+    # Loop through files in the directory
     dir_root=`ls -pd ${root_dir}nmea_data/* | grep "/$"`
     for directory in $dir_root; do
         for filename in $(ls -1 "$directory"); do
           filepath="$directory$filename"
-          # is it is a .nmea file
-          if [ "$filename" != "${filename%.nmea}" ]; then #ash idiom
+          # is it is a .nmea file ?
+          if [ "$filename" != "${filename%.nmea}" ]; then # ash idiom
               # here it DOES end in .nmea
               # NB includes .day.nmea
               # Check if it's a regular file (skip directories, etc.)
@@ -72,29 +76,37 @@ else
                 # Get the file modification time
                 file_mtime=$(date -r "$filepath" +%s)
                 file_stamp=$(date -r "$filepath" +"%F %T %Z")
+                
+                if [ $file_mtime -gt $youngest_mtime ]; then
+                  youngest_mtime=$file_mtime
+                  youngest_fn=$filename
+                fi
 
                 # Compare with threshold time
-                if [ $file_mtime -lt $threshold_time ]; then
+                if [ $file_mtime -gt $threshold_time ]; then
+                  # it is 
                   # echo "DIFFERENCE=$(( $file_mtime - $threshold_time ))"
-                  overdue_updated=1
+                  overdue_updated=0
                   updated=$filename
                   update_dir=$directory
                 fi
                 # Compare with usual time
-                if [ $file_mtime -lt $usual_time ]; then
-                  # echo "DIFFERENCE=$(( $file_mtime - $usual_time ))"
-                  usual_updated=1
+                if [ $file_mtime -gt $usual_time ]; then
+                  echo " .nmea file found $file_stamp, but checked all against limits"
+                  usual_DIFFERENCE=$(( $file_mtime - $usual_time ))
+                  usual_updated=0
                   usual_fn=$filename
                   usual_dir=$directory
                 fi
               fi
           else
-          # files which do NOT have the .nmea extension
+            # files which do NOT have the .nmea extension
             continue
           fi
         done
     done
-    echo "NOT the Youngest file found: $nmeafilepath dated $file_stamp, but checked all against limits"
+    echo "YOUNGEST    $youngest_mtime $youngest_fn"
+
 fi
 
 
@@ -102,7 +114,7 @@ fi
 stillalive=0 # flag to see if the heartbeat still_alive.txt is still alive =0 means alive
 alivepath="${root_dir}nmea_logs/still_alive.txt"
 
-if [ -f "$alivepath" ]; then
+if [ -f $alivepath ]; then
     alive_mtime=$(date -r "$alivepath" +%s)
     alive_stamp=$(date -r "$alivepath" +"%F %T %Z")
     # echo "There is an alive timestamp:$alive_stamp mod time:$alive_mtime thresh_time:$threshold_time"
@@ -130,11 +142,11 @@ if [ $overdue_updated -ne 0 ]; then
   touch ${root_dir}nmea_logs/nmealogger-hung.txt     
   # we are overdue, but is there a keep_alive? 
   if [ $stillalive -ne 0 ]; then
-      echo `date` "Alive: but no update in $threshold minutes. $updated $file_stamp nmeachecker.sh" >> ${root_dir}nmea_logs/nmealogger_error.txt
+      echo `date` "Alive: but no .nmea update in $threshold minutes. $updated $file_stamp nmeachecker.sh" >> error_file
   else
       # so kill the .py process, which terminates the .sh script
       # cron will then restart it in 3 minutes
-      echo `date` "Hung: no update in $threshold minutes.  $updated $file_stamp nmeachecker.sh" >> ${root_dir}nmea_logs/nmealogger_error.txt
+      echo `date` "Hung: no .nmea update in $threshold minutes.  $updated $file_stamp nmeachecker.sh" >> error_file
       pkill -ef "python /root/nmea_gps/nmealogger.py"
       exit 1
   fi
@@ -142,7 +154,7 @@ fi
 
 # if it is between 1 and 13 minutes overdue, just make a note.
 if [ $usual_updated -ne 0 ]; then
-    echo `date` "No files in '$directory' have been updated in the last $usual minutes."
-    echo `date` "Slow: no update in $usual minutes.  $usual_fn $file_stamp nmeachecker.sh" >> ${root_dir}nmea_logs/nmealogger_error.txt
+    echo `date` "No .nmea files in '$directory' have been updated in the last $usual minutes. "
+    echo `date` "Slow: no .nmea update in $usual minutes.  nmeachecker.sh $youngest_mtime $youngest_fn" >> error_file
 fi
 exit 0
